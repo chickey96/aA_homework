e3e95db6b5116d3e7967e02774e1d2f6338b9677
Corinne <c.m.hickey06@gmail.com>
2019-02-19 07:07:45 -0800

    Add phase one and two of middleware homework

---
+++ W7D2/frontend/todo_redux.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+  JSON.parse(localStorage.state) : {};
+  let store = configureStore(preloadedState);
+  store = applyMiddlewares(store, [addLoggingToDispatch]);
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
+
+const addLoggingToDispatch = store => next => action => {
+    console.log("Current State: ", store.getState());
+    console.log("Action: ", action);
+    let track = next(action);
+    console.log("New State: ", store.getState());
+    return track;
+}
+
+const applyMiddlewares = (store, middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach(middleware => {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch });
+}
+
+
+
+  
+
+
